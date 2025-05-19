26bd212588e99a812650d18e7705f45660761d5a
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-05-19 12:26:19 +0800

    kernel: sucompat: increase reliability and micro-optimize
    This is a squash of the following:
    - kernel: ksud, sucompat: micro-opt, drop redundant memset before strncpy
    - kernel: sucompat: micro-opt exec handlers - move permission check up
    - kernel: sucompat: increase reliability of execve_sucompat
    - kernel: sucompat: allow pagefults on syscall sucompat handlers
    
    On plain ARMv8.0 devices (A53,A57,A73), strncpy_from_user_nofault() sometimes
    fails to copy `filename_user` string correctly. This breaks su ofc, breaking
    some apps like Termux (Play Store ver), ZArchiver and Root Explorer.
    
    This does NOT seem to affect newer ARMv8.2+ CPUs (A75/A76 and newer)
    
    My speculation? ARMv8.0 has weak speculation :)
    
    here we replace `strncpy_from_user_nofault()` with another routine:
    - access_ok() to validate the pointer
    - strncpy_from_user() to copy and validate string
    - manual null-termination just in case, as strncpy_from_user_nofault also does it
    - remove that memset, seems useless as it is an strncpy, not strncat
    
    basically, we allow pagefaults.
    
    for usercopies, its not like were doing
            memset(dest, 0, sizeof(dest));
            strncat(dest, var, bytes);
    
    that memset seems unneeded. instead we use strncpy itself to do proper
    error and oob check and null term it after.
    
    as for exec handlers, just return early if unauthorized
    
    EDIT: add access_ok compat handling too
    
    Tested on:
    - ARMv8.0 A73.a53, A57.a53, A53.a53
    - ARMv8.2 A76.a55
    
    Tested-by: iDead XD <rafifirdaus12bb@gmail.com>
    Tested-by: rsuntk <rsuntk@yukiprjkt.my.id>
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>
    
    -https://github.com/backslashxx/KernelSU/issues/5
    __kernel_compat: remove earlier ksu_access_ok (https://github.com/pershoot/KernelSU-Next/commit/3965a25a8f282cd389b2fdc19c104021f37c68d9),
      re-construct and place right before its invocation in ksu_copy_from_user_nofault
    -https://gitlab.com/pershoot/susfs4ksu/-/tree/gki-android14-6.1-dev

--- kernel/sucompat.c
+++ kernel/sucompat.c
@@ -57,6 +57,16 @@ static char __user *ksud_user_path(void)
 	return userspace_stack_buffer(ksud_path, sizeof(ksud_path));
 }
 
+static __attribute__((hot)) long ksu_strncpy_from_user_checked(char *dst,
+		        const void __user *unsafe_addr, long count)
+
+{
+	if (unlikely(!ksu_access_ok(unsafe_addr, count)))
+		return -EFAULT;
+
+	return strncpy_from_user(dst, unsafe_addr, count);
+}
+
 int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
 			 int *__unused_flags)
 {
@@ -71,9 +81,15 @@ int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
 		return 0;
 	}
 
+	if (unlikely(!filename_user))
+		return 0;
+
 	char path[sizeof(su) + 1];
-	memset(path, 0, sizeof(path));
-	ksu_strncpy_from_user_nofault(path, *filename_user, sizeof(path));
+	long len = ksu_strncpy_from_user_checked(path, *filename_user, sizeof(path));
+	if (len <= 0)
+		return 0;
+
+	path[sizeof(path) - 1] = '\0';
 
 	if (unlikely(!memcmp(path, su, sizeof(su)))) {
 		pr_info("faccessat su->sh!\n");
@@ -124,7 +140,6 @@ int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
 	}
 
 	char path[sizeof(su) + 1];
-	memset(path, 0, sizeof(path));
 // Remove this later!! we use syscall hook, so this will never happen!!!!!
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 18, 0) && 0
 	// it becomes a `struct filename *` after 5.18
@@ -139,7 +154,11 @@ int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
 	pr_info("vfs_statx su->sh!\n");
 	memcpy((void *)filename->name, sh, sizeof(sh));
 #else
-	ksu_strncpy_from_user_nofault(path, *filename_user, sizeof(path));
+	long len = ksu_strncpy_from_user_checked(path, *filename_user, sizeof(path));
+	if (len <= 0)
+		return 0;
+
+	path[sizeof(path) - 1] = '\0';
 
 	if (unlikely(!memcmp(path, su, sizeof(su)))) {
 		pr_info("newfstatat su->sh!\n");
@@ -163,6 +182,9 @@ int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
 	}
 #endif
 
+	if (!ksu_is_allow_uid(current_uid().val))
+		return 0;
+
 	if (unlikely(!filename_ptr))
 		return 0;
 
@@ -172,9 +194,6 @@ int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
 	}
 
 	if (likely(memcmp(filename->name, su, sizeof(su))))
-		return 0;
-
-	if (!ksu_is_allow_uid(current_uid().val))
 		return 0;
 
 	pr_info("do_execveat_common su found\n");
@@ -198,6 +217,9 @@ int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user,
 	}
 #endif
 
+	if (!ksu_is_allow_uid(current_uid().val))
+		return 0;
+
 	if (unlikely(!filename_user))
 		return 0;
 
@@ -211,9 +233,9 @@ int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user,
 	if (!access_ok(*filename_user, sizeof(path)))
 		return 0;
 #endif
-	// success = returns number of bytes and should be less than path
-	long len = strncpy_from_user(path, *filename_user, sizeof(path));
-	if (len <= 0 || len > sizeof(path))
+	// success = returns number of bytes
+	long len = ksu_strncpy_from_user_checked(path, *filename_user, sizeof(path));
+	if (len <= 0)
 		return 0;
 
 	// strncpy_from_user_nofault does this too
