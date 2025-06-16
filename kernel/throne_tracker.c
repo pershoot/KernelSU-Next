37b6e9c0d4ca9c4186a0e36d418b4b1b2fbf6de5
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-06-16 18:42:46 +0800

    kernel: implement susfs v1.5.8 KernelSU Next v1.0.8
    -remove some extra spaces from (TG) patch
    -try_umount -> ksu_try_umount if not CONFIG_KSU_SUSFS_TRY_UMOUNT (ksu_handle_setuid)
    
    -7/10/25: selinux/rules: fix merge conflict with previous change (Fix illegal RCU lock usage):
    __https://github.com/pershoot/KernelSU-Next/commit/4d633a1e0e5934148b0de998b06c16e4ae2f0f74
    -7/14/25: core_hook: fix merge conflict with previous change (remove redundant get_cred_rcu):
    __https://github.com/pershoot/KernelSU-Next/commit/bd03c296a1f2af9569dfbb49077068fef1864c9d
    -7/20/25: ksud: fix merge conflict with previous change (guard syscall hook types):
    __https://github.com/pershoot/KernelSU-Next/commit/45ad73e9dd86a0ff04a02e73a8fc2dbc3160ee6c
    -7/20/26: core_hook: fix merge conflict with previous change (support for KernelNoSU):
    __https://github.com/pershoot/KernelSU-Next/commit/948975ba35bebbd6e4b4ab41d33715ce7d79b572
    -7/24/25: core_hook: fix merge conflict with previous change (allow only perms ... and guard kernelnosu perms):
    __https://github.com/pershoot/KernelSU-Next/commit/3f4c23a34f4f2d3011a0bb0b6a14a8f10bd18b7d
    -8/10/25: Kconfig: fix merge conflict with previous change (nest ksun switch manager support)
    __https://github.com/pershoot/KernelSU-Next/commit/e0c461322bca8ab698e3e9749a7d467f67f5daa4
    -9/7/25: core_hook: fix merge conflict with previous change (Unmount isolated process)
    __'Unmount isolated process which forks from zygote unconditionally (https://github.com/tiann/KernelSU/pull/2747)'
    -9/21/25: fix merge conflicts with previous changes (Kconfig, apk_sign, throne_tracker):
    __'kernel: throne_tracker: offload to kthread (tiann#2632)'
    __'kernel: remove KSU_SWITCH_MANAGER logic'
    -9/23/25:
     core_hook: restore previous logic from 9/7/25 (susfs: current process is zygote)
     __implement partial logic from proceeding change:
       'Sync with latest KernelSU commit 4d3560b12bec5f238fe11f908a246f0ac97e9c27'
     sucompat: remove ksud_path identifier (ksud_user_path)
    
    -https://gitlab.com/simonpunk/susfs4ksu/-/tree/gki-android14-6.1
    __ref: https://gitlab.com/sidex15/KernelSU-Next

--- kernel/throne_tracker.c
+++ kernel/throne_tracker.c
@@ -196,7 +196,7 @@ FILLDIR_RETURN_TYPE my_actor(struct dir_context *ctx, const char *name,
 				}
 			}
 
-			bool is_manager = is_manager_apk(dirpath);
+			bool is_manager = ksu_is_manager_apk(dirpath);
 			pr_info("Found new base.apk at path: %s, is_manager: %d\n",
 				dirpath, is_manager);
 			if (is_manager) {
@@ -472,7 +472,7 @@ static int throne_tracker_thread(void *data)
 	return 0;
 }
 
-void track_throne()
+void ksu_track_throne()
 {
 #ifndef CONFIG_KSU_THRONE_TRACKER_ALWAYS_THREADED
 	static bool throne_tracker_first_run __read_mostly = true;
