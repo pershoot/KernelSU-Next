762f126d5900cfff0a5b851ba021ec5126f34d01
pershoot <190600+pershoot@users.noreply.github.com>
2025-07-14 04:36:00 -0400

    kernel: bring the rest of the security hooks under LSM
    -original change:
    __https://github.com/pershoot/KernelSU-Next/commit/217d230b61b3297b93b511d766e5ff1896886f06
    __https://github.com/pershoot/KernelSU-Next/next-susfs
    ____'kernel: ksud, core_hook: migrate ksud execution to security_bprm_check'
    -kernel: expose KSU_LSM_SECURITY_HOOKS on Kconfig
    __https://github.com/backslashxx/KernelSU
    -7/20/25: core_hook: fix merge conflict with previous change (support for KernelNoSU):
    __https://github.com/pershoot/KernelSU-Next/commit/948975ba35bebbd6e4b4ab41d33715ce7d79b572
    -10/30/25: core_hook: fix merge conflict with previous change (replace renameat hook)

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -326,7 +326,7 @@ static bool is_system_bin_su(void)
     return false;
 }
 
-int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
+LSM_HANDLER_TYPE ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
 		     unsigned long arg4, unsigned long arg5)
 {
 	// if success, we modify the arg5 as result!
@@ -1094,7 +1094,7 @@ void susfs_try_umount_all(uid_t uid) {
 }
 #endif
 
-int ksu_handle_setuid(struct cred *new, const struct cred *old)
+LSM_HANDLER_TYPE ksu_handle_setuid(struct cred *new, const struct cred *old)
 {
 	// this hook is used for umounting overlayfs for some uid, if there isn't any module mounted, just ignore it!
 	if (!ksu_module_mounted) {
@@ -1287,7 +1287,7 @@ __maybe_unused int ksu_kprobe_exit(void)
 
 extern int ksu_handle_devpts(struct inode *inode); // sucompat.c
 
-int ksu_inode_permission(struct inode *inode, int mask)
+LSM_HANDLER_TYPE ksu_inode_permission(struct inode *inode, int mask)
 {
 	if (inode && inode->i_sb
 		&& unlikely(inode->i_sb->s_magic == DEVPTS_SUPER_MAGIC)) {
@@ -1299,7 +1299,7 @@ int ksu_inode_permission(struct inode *inode, int mask)
 
 // kernel 4.9 and older
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) || defined(CONFIG_IS_HW_HISI) || defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)
-int ksu_key_permission(key_ref_t key_ref, const struct cred *cred,
+LSM_HANDLER_TYPE ksu_key_permission(key_ref_t key_ref, const struct cred *cred,
 			      unsigned perm)
 {
 	if (init_session_keyring != NULL) {
